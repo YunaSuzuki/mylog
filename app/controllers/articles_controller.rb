9743dc61b35f07a098c7ef9470df19fde650cc3f
yuna.suzuki <yuna.com.jp@outlook.jp>
2020-04-28 02:29:05 +0000

    edit controller(show) & add show-view & edit scss

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all.order(created_at: :desc) 
   end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
 end
