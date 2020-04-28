cd81bef146a4c03cf178546b12b728fddc3a9051
yuna.suzuki <yuna.com.jp@outlook.jp>
2020-04-28 06:45:03 +0000

    add post-function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,20 @@ class ArticlesController < ApplicationController
   end
   
   def new
+    @article = Article.new
+  end
+  
+  def create
+    Article.create(title: article_params[:title], content: article_params[:content])
+    redirect_to :root
   end
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  private
+  def article_params
+    params.permit(:title, :content)
   end
 end
